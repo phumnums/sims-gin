92f303a0126f787bb9647ebd197e946888b9e195
Sirintra Phumnum <ph.sirintra@gmail.com>
2026-05-05 14:06:53 +0700

    refactor: postgres db

--- internal/infarstructure/database/postgres.go
+++ internal/infarstructure/database/postgres.go
@@ -1,12 +1,15 @@
 package database
 
 import (
+	"cims/internal/infarstructure/config"
+	"fmt"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func NewPostgresDB() (*gorm.DB, error) {
-	dsn := "host=localhost user=postgres password=password dbname=sims_db port=5432 sslmode=disable"
+func NewPostgresDB(cfg *config.Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", cfg.Database.Host, cfg.Database.User, cfg.Database.Password, cfg.Database.Name, cfg.Database.Port, cfg.Database.SSLMode)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
